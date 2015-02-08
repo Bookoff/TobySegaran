ee0e1a86ee8f0f0e9bf580e902a015c449b935f0
Bookoff <bookoff@thebookoff.ru>
2015-02-08 22:29:56 +0300

    Рекомендование предметов

--- Recommendations/recommendations.py
+++ Recommendations/recommendations.py
@@ -78,3 +78,33 @@ def topMatches(prefs, person, n=5, similarity=sim_pearson):
 	scores.sort()
 	scores.reverse()
 	return scores[0:n]
+
+
+# Получить рекомендации для заданного человека, пользуясь взвешенным средним оценок, данных всеми остальными пользователями
+def getRecommendations(prefs,person,similarity=sim_pearson):
+	totals={}
+	simSums={}
+	for other in prefs:
+		# сравнивать меня с собой же не нужно
+		if other==person: continue
+		sim=similarity(prefs,person,other)
+
+		# игнорировать нулевые и отрицательные оценки
+		if sim<=0: continue
+		for item in prefs[other]:
+			# оценивать только фильмы, которые я еще не смотрел
+			if item not in prefs[person] or prefs[person][item]==0:
+				# Коэффициент подобия * Оценка
+				totals.setdefault(item,0)
+				totals[item] += prefs[other][item]*sim
+				# Сумма коэффициентов подобия
+				simSums.setdefault(item,0)
+				simSums[item] += sim
+
+	# Создать нормализованный список
+	rankings=[(total/simSums[item],item) for item,total in totals.items()]
+
+	# Вернуть отсортированный список
+	rankings.sort()
+	rankings.reverse()
+	return rankings
