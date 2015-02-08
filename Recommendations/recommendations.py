7bb507d609a472f2a6d0507653bc3fbffd66a666
Bookoff <bookoff@thebookoff.ru>
2015-02-08 21:33:26 +0300

    Функция подсчета коэффициента Пирсона

--- Recommendations/recommendations.py
+++ Recommendations/recommendations.py
@@ -32,3 +32,38 @@ def sim_distance(prefs, person1, person2):
 	sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
 
 	return 1/(1+sum_of_squares)
+
+
+#  Возвращает коэффициент корреляции Пирсона между p1 и p2
+def sim_pearson(prefs,p1,p2):
+	# Получить список предметов, оцененных обоими
+	si={}
+	for item in prefs[p1]:
+		if item in prefs[p2]: si[item]=1
+	# Найти число элементов
+	n=len(si)
+
+	# Если нет ни одной общей оценки, вернуть 0
+	if n==0: return 0
+
+	# Вычислить сумму всех предпочтений
+	sum1=sum([prefs[p1][it] for it in si])
+	sum2=sum([prefs[p2][it] for it in si])
+
+	# Вычислить сумму квадратов
+	sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+	sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+
+	# Вычислить сумму произведений
+	pSum = sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+	# Вычислить коэффициент Пирсона
+	num=pSum-(sum1*sum2/n)
+	den=sqrt((sum1S1-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+
+	if den==0: return 0
+
+	r=num/den
+
+	return r
+
