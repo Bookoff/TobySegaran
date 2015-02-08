1bfeba7c5f4af38591e8de27339aca9add613647
Bookoff <bookoff@thebookoff.ru>
2015-02-08 21:15:25 +0300

    Оценка подобия на основе расстояния

--- Recommendations/recommendations.py
+++ Recommendations/recommendations.py
@@ -12,3 +12,23 @@ critics={
 'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5}, 
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}
 }
+
+from math import sqrt
+
+
+# Возвращает оценку подобия person1 и person2 на основе расстояния
+
+def sim_distance(prefs, person1, person2):
+	# Получить список предметов, оцененных обоими
+	si = {}
+	for item in prefs[person1]:
+		if item in prefs[person2]:
+			si[item]=1
+
+	# Если нет ни одной общей оценки, вернуть 0
+	if len(si)==0: return 0
+
+	# Сложить квадраты разностей
+	sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
+
+	return 1/(1+sum_of_squares)
